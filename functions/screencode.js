532ab8fffff8eb1ef76dd1e321532facddb6d397
Damian Soto <soto.platero@gmail.com>
2020-12-01 18:49:14 -0500

    screencode

--- functions/screencode.js
+++ functions/screencode.js
@@ -1,7 +1,7 @@
 const chromium = require('chrome-aws-lambda');
 const hljs = require('highlight.js');
 const prettier = require("prettier");
-const Prism = require('prismjs');
+// const Prism = require('prismjs');
 
 // the browser path
 const localChrome = process.env.PATH_CHROME;
@@ -9,7 +9,7 @@ const localChrome = process.env.PATH_CHROME;
 exports.handler = async (event, context) => {
 
     let {lang, code, theme} = event.queryStringParameters;
-    console.log(event.headers.host)
+
     theme = theme || 'androidstudio'
 
     try {
@@ -33,21 +33,20 @@ exports.handler = async (event, context) => {
         let content = `
             <!doctype html>
             <head>
-            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.5/styles/${theme}.min.css">
-            <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.0/highlight.min.js"></script>
-            <script>hljs.initHighlightingOnLoad();</script>
-            <style>pre{display: inline-block;font-size:1.3rem;}</style>
-            </head>
-            <body>
-            <pre><code>${highlightedCode}</code></pre>
-            </body>
-            </html>`;
+                <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.5/styles/${theme}.min.css">
+                <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.0/highlight.min.js"></script>
+                <script>hljs.initHighlightingOnLoad();</script>
+                <style>pre{display: inline-block;font-size:1.3rem;}</style>
+            </head><body>
+                <pre><code>${highlightedCode}</code></pre>
+            </body></html>`;
     
         await page.setContent(content);
     
         const elCode = await page.$('pre');
         const screenshot = await elCode.screenshot({ encoding: 'base64' });
         await browser.close();
+
         return {
             statusCode: 200,
             headers: { 'Content-type': 'image/jpeg' },
@@ -58,9 +57,7 @@ exports.handler = async (event, context) => {
     } catch (e) {
 
         return {
-            headers: { 
-                'Content-Type':'application/json' , 
-            },            
+            headers: { 'Content-Type':'application/json'},            
             statusCode: 500,
             body: JSON.stringify({error: e}),   
         }     
