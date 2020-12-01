f225974cd60927128bc2548b15650305e9647163
Damian Soto <soto.platero@gmail.com>
2020-12-01 06:23:05 -0500

    add code function

--- functions/code.js
+++ functions/code.js
@@ -1,12 +1,28 @@
 const chromium = require('chrome-aws-lambda');
 const hljs = require('highlight.js');
+const prettier = require("prettier");
+const Prism = require('prismjs');
 
 // the browser path
 const localChrome = process.env.PATH_CHROME;
 
 exports.handler = async (event, context) => {
 
-    const url = event.queryStringParameters.url 
+    const {language,code} = event.queryStringParameters;
+    
+    let htmlStart = `
+        <!doctype html>
+        <head>
+
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.22.0/themes/prism.min.css" rel="stylesheet" />
+        </head>
+        <body>`
+
+    let htmlEnd = `
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.22.0/prism.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.22.0/plugins/autoloader/prism-autoloader.min.js"></script>        
+
+    </body></html>`
 
     const browser = await chromium.puppeteer.launch({
         ignoreDefaultArgs: ['--disable-extensions'],
@@ -19,11 +35,13 @@ exports.handler = async (event, context) => {
     const page = await browser.newPage();
     await page.goto('http://localhost:8888/blank.html', { waitUntil: 'networkidle2' });
 
-    const highlightedCode = hljs.highlightAuto('<span>Hello World!</span>').value
-    await page.setContent(highlightedCode);
-  
+    let prettierCode = prettier.format(code, { parser: "babel" });
 
-    const screenshot = await page.screenshot({ encoding: 'base64' });
+    // const highlightedCode = hljs.highlight('js', prettierCode).value
+    const highlightedCode = Prism.highlight(prettierCode, Prism.languages.javascript, 'javascript');
+    await page.setContent(htmlStart +'<pre style="padding: 1rem; display: inline-block;"><code>' +  highlightedCode + '</code></pre>' + htmlEnd);
+    const elCode = await page.$('pre');
+    const screenshot = await elCode.screenshot({ encoding: 'base64' });
     await browser.close();
     
     return {
