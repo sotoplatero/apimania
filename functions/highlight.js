afd56ef9b4e3af6e41315132338d9bb6c2cafd62
Damian Soto <soto.platero@gmail.com>
2020-12-03 06:17:12 -0500

    prettier standalone

--- functions/highlight.js
+++ functions/highlight.js
@@ -1,5 +1,8 @@
 const hljs = require("highlight.js/lib/core");
-const prettier = require("prettier");
+const prettier = require("prettier/standalone");
+const parserBabel = require("prettier/parser-babel");
+const parserHtml = require("prettier/parser-html");
+const parserPostcss = require("prettier/parser-postcss");
 
 exports.handler = async (event, context) => {
 
@@ -10,7 +13,10 @@ exports.handler = async (event, context) => {
     lang = (lang==='javascript') ? 'babel' : lang;
     // try {
     
-        let prettierCode = prettier.format(code, { parser: lang });
+        let prettierCode = prettier.format(code, { 
+            parser: lang, 
+            plugins: [parserBabel, parserHtml, parserPostcss], 
+        });
 
         hljs.registerLanguage(lang, require('highlight.js/lib/languages/xml'));        
         const highlightedCode = hljs.highlight(lang, prettierCode).value
