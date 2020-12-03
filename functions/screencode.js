9beb06c3811a0a7ca46c32fac2e5546b408830a0
Damian Soto Platero <jefe.redes@gaviota.cu>
2020-12-03 09:44:01 -0500

    all in browser puppeteer

--- functions/screencode.js
+++ functions/screencode.js
@@ -18,16 +18,10 @@ exports.handler = async (event, context) => {
     theme = theme || 'androidstudio'
     lang = lang || 'babel'
     lang = (lang==='javascript') ? 'babel' : lang;
-    console.log(code)
-    // hljs.registerLanguage(lang, require('highlight.js/lib/languages/xml'));
 
     // try {
 
-        const host = (process.env.ENV === 'local') ? 'http://localhost:8888' : 'https://apimania.netlify.com'
-        // const response = await got( host + `/.netlify/functions/highlight?code=${code}&lang=${lang}`)
-        // const highlightedCode = response.body
-        // console.log(response.body)
-
+        
         const browser = await chromium.puppeteer.launch({
             ignoreDefaultArgs: ['--disable-extensions'],
             args: chromium.args,
@@ -36,35 +30,24 @@ exports.handler = async (event, context) => {
             headless: chromium.headless,
         });
         
+        // Open page base
         const page = await browser.newPage();
-        await page.goto( host + `/blank.html?code=${code}$lang=${lang}`, { waitUntil: 'networkidle0' });
-        
-        let content = `
-            <!doctype html>
-            <head>
-            <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.5/styles/${theme}.min.css">
-            <script src="https://unpkg.com/prettier@2.2.1/standalone.js"></script>
-            <script src="https://unpkg.com/prettier@2.2.1/parser-babel.js"></script>
-            <script src="https://unpkg.com/prettier@2.2.1/parser-html.js"></script>
-            <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.0/highlight.min.js"></script>
-            <style>pre{display: inline-block;font-size:1.3rem;}</style>
-            </head><body>
-            <pre><code></code></pre>
-            <script>
-                var prettierCode = prettier.format( "${code}", {
-                    parser: "${lang}",
-                    plugins: prettierPlugins,
-                });
-                document.querySelector('code').innerHTML = prettierCode;
-                hljs.initHighlightingOnLoad();
-            </script>
-            </body></html>
-        `;
-        
-        // page.goto(`data:text/html,${content}`);
-        // await page.setContent(content);
-        // page.waitForNavigation()
+        const host = (process.env.ENV === 'local') ? 'http://localhost:8888' : 'https://apimania.netlify.com'
+        await page.goto( host + `/blank.html`,{ waitUntil: 'networkidle0' });
+
+        await page.evaluate( ( code, lang ) => {
+    
+            var prettierCode = prettier.format( code, {
+                parser: lang,
+                plugins: prettierPlugins,
+            });
+    
+            let codeEle = document.querySelector('pre code')
+            codeEle.innerHTML =  prettierCode;
+            hljs.highlightBlock(codeEle);   
+
+        }, code, lang)
+
         const elCode = await page.$('pre');
         const screenshot = await elCode.screenshot({ encoding: 'base64' });
         await browser.close();
